ab333d461ba197852270bf30d5b3d76fc8df4953
borisbokan <borisbokan@gmail.com>
2025-11-10 22:00:07 +0100

    Fix Js for emal sending

--- js/email_send.js
+++ js/email_send.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('.contact-form');
     // URL Vašeg Worker-a
-    const workerUrl = "https://g4construction-mail-handler2.borisbokan.workers.dev"; // PROMENITE OVO AKO VAM SE URL RAZLIKUJE
+    const workerUrl = "https://mail-handler-g4con.g4build-llc.workers.dev/"; // PROMENITE OVO AKO VAM SE URL RAZLIKUJE
 
     form.addEventListener('submit', function(e) {
         e.preventDefault(); // Sprečavamo standardno slanje forme i redirect
