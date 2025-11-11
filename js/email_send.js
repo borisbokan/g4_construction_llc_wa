a6521787761dad0816502294903e5f58821c039a
borisbokan <borisbokan@gmail.com>
2025-11-11 18:56:10 +0100

    Change URL to first one

--- js/email_send.js
+++ js/email_send.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('.contact-form');
     // KORISTIMO CUSTOM ROUTE:
-    const workerUrl = "https://broken-cherry-f9d1.g4build-llc.workers.dev"; 
+    const workerUrl = "https://form-api.g4-construction.com"; 
 
     form.addEventListener('submit', function(e) {
         e.preventDefault(); 
