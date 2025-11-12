9783a9e7cfb44ac25915d577bbc856706b5e0537
borisbokan <borisbokan@gmail.com>
2025-11-12 08:32:57 +0100

    bac to free plan

--- js/email_send.js
+++ js/email_send.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('.contact-form');
     // KORISTIMO CUSTOM ROUTE:
-    const workerUrl = "https://form-api.g4-construction.com"; 
+    const workerUrl = "https://email-handler-g4con.g4build-llc.workers.dev"; 
 
     form.addEventListener('submit', function(e) {
         e.preventDefault(); 
